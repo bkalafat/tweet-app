0d18f1015abaab81e4e93e65c50e0b227b325807
Burak Kalafat <kalafatburak@gmail.com>
2019-05-18 12:48:58 +0300

    feat: Handle initial data when app mounted.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,19 +1,17 @@
 import { getInitialData } from '../utils/api'
-import { receiveTweets } from './tweets'
-import { receiveUsers } from './users'
-import {setAuthedUsers} from './authedUser'
+import { receiveUsers } from '../actions/users'
+import { receiveTweets } from '../actions/tweets'
+import { setAuthedUser } from '../actions/authedUser'
 
 const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData () {
-  return () => {
-    return (dispatch) => {
-      return getInitialData()
-      .then(({users, tweets})=> {
+  return (dispatch) => {
+    return getInitialData()
+      .then(({ users, tweets }) => {
         dispatch(receiveUsers(users))
         dispatch(receiveTweets(tweets))
-        dispatch(setAuthedUsers(AUTHED_ID))
+        dispatch(setAuthedUser(AUTHED_ID))
       })
-    }
   }
 }
--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,12 +1,11 @@
-import {
-  SET_AUTHED_USER
-} from '../actions/authedUser'
 
-export default function authedUser(state = null, action) {
+import { SET_AUTHED_USER } from '../actions/authedUser'
+
+export default function authedUser (state = null, action) {
   switch (action.type) {
-    case SET_AUTHED_USER:
+    case SET_AUTHED_USER :
       return action.id
-    default:
+    default :
       return state
   }
 }
