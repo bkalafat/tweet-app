e5f036c216654b239ee682b374064c49875c11fc
Burak Kalafat <kalafatburak@gmail.com>
2019-05-24 23:55:49 +0300

    feat: Add icons for tweet UI

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import {connect } from 'react-redux'
 import { formatTweet } from '../utils/helpers'
-
+import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline} from 'react-icons/ti/index'
 
 class Tweet extends Component {
   render () {
