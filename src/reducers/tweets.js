083d62dd75cdfff0ea05231518ab671c58dad749
Burak Kalafat <kalafatburak@gmail.com>
2019-05-25 14:06:39 +0300

    feat: Toggle authenticated user in likes array.

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,5 +1,5 @@
 import {
-  RECEIVE_TWEETS
+  RECEIVE_TWEETS, TOGGLE_TWEET
 } from '../actions/tweets'
 
 export default function tweets(state = {}, action) {
@@ -8,6 +8,16 @@ export default function tweets(state = {}, action) {
       return {
         ...state,
         ...action.tweets
+      }
+    case TOGGLE_TWEET:
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          likes: action.hasLiked === true
+            ? state[action.id].likes.filter((uid) => uid !== action.authedUser)
+            : state[action.id].likes.concat(action.authedUser)
+        }
       }
       default:
         return state
