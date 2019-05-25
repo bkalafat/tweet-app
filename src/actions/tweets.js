f1b76ff0496fe89a037e40442bf2d192f7b1478e
Burak Kalafat <kalafatburak@gmail.com>
2019-05-25 13:21:42 +0300

    feat: Toggle tweet like when heart pressed.

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,8 +1,33 @@
+import { saveLikeToggle } from '../utils/api'
+
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
+export const TOGGLE_TWEET = 'TOGGLE_TWEET'
 
 export function receiveTweets (tweets) {
   return {
     type: RECEIVE_TWEETS,
     tweets
+  }
+}
+
+function toggleTweet ({id, authedUser, hasLiked }) {
+  return {
+    type: TOGGLE_TWEET,
+    id,
+    authedUser,
+    hasLiked
+  }
+}
+
+export function handleToggleTweet (info) {
+  return (dispacth) => {
+    dispacth(toggleTweet(info))
+
+    return saveLikeToggle(info)
+    .catch((e)=> {
+      console.warn('Error ib handleToggleTweet', e)
+      dispacth(toggleTweet(info))
+      alert('There was an error liking the tweet. Try again')
+    })
   }
 }
